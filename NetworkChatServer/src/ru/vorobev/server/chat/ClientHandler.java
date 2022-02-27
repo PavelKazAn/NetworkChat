78cf424ef9c0db8d647d747acaf4b95edd0568b6
PavelKazAn <frozzty@yandex.ru>
2022-02-27 14:38:34 +0300

    Добавил мавен и доработал

---
+++ NetworkChatServer/src/ru/vorobev/server/chat/ClientHandler.java
@@ -0,0 +1,67 @@
+package ru.vorobev.server.chat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    private MyServer server;
+    private final Socket clientSocket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+
+    public ClientHandler(MyServer myServer, Socket clientSocket) {
+        this.server = myServer;
+        this.clientSocket = clientSocket;
+    }
+
+    public void handle() throws IOException {
+        inputStream = new DataInputStream(clientSocket.getInputStream());
+        outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+        new Thread(() -> {
+            try {
+                server.subscribe(this);
+                readMessages();
+            } catch (IOException e) {
+                System.err.println("Failed to process message from client ");
+                e.printStackTrace();
+            } finally {
+                try {
+                    closeConnection();
+                } catch (IOException e) {
+                    System.err.println("Failed to close connection");
+                }
+            }
+        }).start();
+
+    }
+
+    private void readMessages() throws IOException {
+        while (true) {
+            String message = inputStream.readUTF().trim();
+            System.out.println("message = " + message);
+            if (message.startsWith("/end")) {
+                return;
+            } else {
+                processMessage(message);
+            }
+        }
+    }
+
+    private void processMessage(String message) throws IOException {
+        this.server.broadcastMessage(message,this);
+    }
+
+    public void sendMessage(String message) throws IOException {
+        this.outputStream.writeUTF(message);
+
+    }
+
+    private void closeConnection() throws IOException {
+        server.unsubscribe(this);
+        clientSocket.close();
+    }
+}
---
+++ src/chat/client/ClientChat.java
@@ -0,0 +1,78 @@
+package chat.client;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientChat extends Application {
+
+    public static final String SERVER_HOST = "localhost";
+    public static final int SERVER_PORT = 8189;
+    public static final String CONNECTION_ERROR_MESSAGE = "Невозможно установить сетевое соеденение";
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        this.stage = stage;
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("chat-template.fxml"));
+
+        Parent load = fxmlLoader.load();
+        Scene scene = new Scene(load);
+
+        this.stage.setTitle("Онлайн чат");
+        this.stage.setScene(scene);
+
+        ClientController controller = fxmlLoader.getController();
+        controller.userList.getItems().addAll("u1", "u2", "u3");
+        stage.show();
+
+        connectToServer(controller);
+    }
+
+    private void connectToServer(ClientController clientController) {
+
+        Network network = new Network();
+        boolean result = network.connect();
+
+        if (!result) {
+            String errorMessage = CONNECTION_ERROR_MESSAGE;
+            System.err.println(CONNECTION_ERROR_MESSAGE);
+            showErrorDialog(CONNECTION_ERROR_MESSAGE);
+            return;
+        }
+
+        clientController.setNetwork(network);
+        clientController.setApplication(this);
+
+        this.stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                network.close();
+            }
+        });
+    }
+
+    public void showErrorDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Ошибка");
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+}
